4eeaea90ff142f6372d33c4407ce8770cbffb7d6
RitikaTiwari1911 <spk2ritika1911@gmail.com>
2021-04-11 19:18:20 +0530

    ADDED: validation for first name

--- userRegistration.sh
+++ userRegistration.sh
@@ -1,3 +1,15 @@
 #!bin/bash
 
 echo "Welcome to the user registration problem"
+
+read -p "Enter the first name, make the first alphabet in Uppercase: " firstName
+
+pat="^[A-Z]{1}[a-z]{2,}"
+
+if [[ $firstName =~ $pat ]]
+then
+        echo "First name is: " $firstName; 
+else
+        echo "wrong input";
+fi
+
