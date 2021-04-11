116fa34cfb13d510e230217ef7fc107838ff5ea8
RitikaTiwari1911 <spk2ritika1911@gmail.com>
2021-04-11 19:20:44 +0530

    ADDED: validation for last name

--- userRegistration.sh
+++ userRegistration.sh
@@ -3,6 +3,7 @@
 echo "Welcome to the user registration problem"
 
 read -p "Enter the first name, make the first alphabet in Uppercase: " firstName
+read -p "Enter the last name, make the first alphabet in Uppercase: " lastName
 
 pat="^[A-Z]{1}[a-z]{2,}"
 
@@ -13,3 +14,9 @@ else
         echo "wrong input";
 fi
 
+if [[ $lastName =~ $pat ]]
+then
+	echo "Last name is: " $lastName;
+else
+	echo "wrong input"
+fi
